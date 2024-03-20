1a8ffa4ab1e0c6f53c5aca51cd412447084bd018
AmonCheruiyot <161847144+AmonCheruiyot@users.noreply.github.com>
2024-03-20 12:59:06 +0300

    completes test

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2; 
+
+console.log(multiply);
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+
+const num3 = 18;
+const num4 = 7;
+
+const mod = (num3 % num4);
+
+console.log(mod);
+
+const set = [5, 12, 20, 8, 15];
+
+const max = Math.max(...set);
+
+console.log(max);
